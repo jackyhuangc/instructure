2e05add9b254e372d64a62d1e834a380f798831b
huangchao <huangchao@kuainiugroup.com>
2018-06-05 14:45:12 +0800

    添加实体

---
+++ strive-dao/src/main/java/com/jacky/strive/dao/model/Member.java
@@ -0,0 +1,85 @@
+package com.jacky.strive.dao.model;
+
+import java.util.Date;
+
+public class Member {
+    private Integer memberId;
+
+    private String memberNo;
+
+    private String memberName;
+
+    private String memberType;
+
+    private Integer memberQuotas;
+
+    private Integer memberPoints;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public Integer getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(String memberNo) {
+        this.memberNo = memberNo == null ? null : memberNo.trim();
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName == null ? null : memberName.trim();
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String memberType) {
+        this.memberType = memberType == null ? null : memberType.trim();
+    }
+
+    public Integer getMemberQuotas() {
+        return memberQuotas;
+    }
+
+    public void setMemberQuotas(Integer memberQuotas) {
+        this.memberQuotas = memberQuotas;
+    }
+
+    public Integer getMemberPoints() {
+        return memberPoints;
+    }
+
+    public void setMemberPoints(Integer memberPoints) {
+        this.memberPoints = memberPoints;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
