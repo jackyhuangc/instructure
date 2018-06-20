5724f986126df10d7d8ec3adc1a7dbf63c8ba857
huangchao <huangchao@kuainiugroup.com>
2018-06-20 22:35:42 +0800

    security 权限

--- strive-service/src/main/java/com/jacky/strive/service/enums/OrderStatusEnum.java
+++ strive-service/src/main/java/com/jacky/strive/service/enums/OrderStatusEnum.java
@@ -7,27 +7,34 @@ package com.jacky.strive.service.enums;
  **/
 public enum OrderStatusEnum {
 
-    // 关键显示，进行中(0.2.4.7)/待发货(3)/待收货(6)/已完成(1.5.8.9)
-    STATUS_NEW(0, "新订单，待支付(已下单)，进行中"),
-    STATUS_CLOSED(1, "已关闭(完成订单)，已完成"),
-    STATUS_PAID(2, "已支付(已支付-未确认退/兑/发)，进行中"),
+    // 正常电商应有状态：0,1,2,4,6,7,8,9,10,11.12
+    // 分享电商应有状态：3.4.5.6.7.8.9.10.11.12
+
+    // 关键显示，进行中(待支付或待确认)(0.3)/待发货(2.5)/待收货(6)/已完成(1.7)/退货(退款)(4.8.9.10.11.12)
+    STATUS_NEW(0, "新订单，待支付(已下单)，订单进行中"),
+    STATUS_CLOSED(1, "已关闭，订单取消"),
+
+    STATUS_PAID(2, "已支付(已支付-未确认退/兑/发)，订单进行中"),
 
     // FIXME 如果自动发货确认=false，才有以下3中状态，默认会变成待确认
-    STATUS_CONFIRM(3, "待确认，可选择退款和发货，待发货/待退款"),// 该状态仅仅MM可用
-    STATUS_REVOKING(4, "待退款(已申请退款)，进行中，可能需要审核"),
-    STATUS_DELIVERING(5, "待发货(已申请发货)，进行中"),
+    STATUS_CONFIRM(3, "待确认(请选择退款和发货)，订单进行中"),
+    STATUS_REVOKING(4, "待退款(已申请退款)，订单进行中"),
+    STATUS_DELIVERING(5, "待发货(已申请发货)，订单进行中"),
 
     // 针对自动发货确认=false时，只能处理STATUS_DELIVERING订单，才能发货
     // 针对自动发货确认=true时，只能处理STATUS_PAID订单，才能发货
-    STATUS_DELIVERED(6, "已发货，待收货"),
-    STATUS_RECEIVED(7, "已收货(完成订单)，已完成"),
+    STATUS_DELIVERED(6, "待收货(已发货)，订单进行中"),
+    STATUS_RECEIVED(7, "已收货，订单完成"),
 
-    STATUS_REFUNDING(8, "待退货(已申请退货)，进行中"),
+    STATUS_REFUNDING(8, "待退货(已申请退货)，订单进行中"),
     // 退货时，需要退款，同时完成STATUS_REVOKED操作，事务处理
-    STATUS_REFUNDED(9, "已退货，待退款，可能需要审核"),
-    // 针对自动发货确认=false时，只能处理STATUS_REVOKING订单，才能退款
-    // 针对自动发货确认=true时，只能处理STATUS_PAID订单，才能退款
-    STATUS_REVOKED(10, "已退款(完成订单)，已完成");
+    STATUS_REFUNDED(9, "已退货，等待退款"),
+    STATUS_REFUNDED_REJECT(10, "退货失败，订单完成"),
+
+    // 针对自动发货确认=false时，自动处理STATUS_REVOKING订单，进行退款
+    // 针对自动发货确认=true时，手动处理STATUS_REVOKING订单，才能退款
+    STATUS_REVOKED(11, "已退款，订单完成"),
+    STATUS_REVOKED_REJECT(12, "退款失败，订单完成");
 
     private Integer value;
 
