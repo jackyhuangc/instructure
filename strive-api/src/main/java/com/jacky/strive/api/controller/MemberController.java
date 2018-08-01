58f0f66e933fd64c1325107d1e32b7246fbf9771
huangchao <huangchao@kuainiugroup.com>
2018-08-01 19:48:20 +0800

    优化上传路径

--- strive-api/src/main/java/com/jacky/strive/api/controller/MemberController.java
+++ strive-api/src/main/java/com/jacky/strive/api/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.jacky.strive.api.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.jacky.strive.dao.model.Member;
-import com.jacky.strive.service.dto.MemberLoginDto;
+import com.jacky.strive.service.MemberService;
 import com.jacky.strive.service.dto.MemberQueryDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import qsq.biz.common.util.AssertUtil;
 import qsq.biz.scheduler.entity.ResResult;
 
 /**
@@ -15,33 +18,68 @@ import qsq.biz.scheduler.entity.ResResult;
 @RequestMapping("/member")
 public class MemberController {
 
-    @GetMapping("/login")
-    public ResResult login(MemberLoginDto loginDto) {
-        return null;
-    }
-
-    @GetMapping("/query")
-    public ResResult query(MemberQueryDto queryDto) {
-        return null;
-    }
+    @Autowired
+    MemberService memberService;
 
     @GetMapping("/{member_no}")
     public ResResult get(@PathVariable("member_no") String memberNo) {
-        return null;
+
+        Member m = memberService.findByMemberNo(memberNo);
+        AssertUtil.notNull(m, "用户不存在");
+
+        return ResResult.success("", m);
     }
 
     @PostMapping("/register")
-    public ResResult register(Member member) {
-        return null;
+    public ResResult create(@RequestBody Member member) {
+
+        Member m = memberService.add(member);
+        AssertUtil.notNull(m, "注册失败");
+
+        return ResResult.success("", m);
     }
 
-    @PutMapping("/modify/{member_no}")
-    public ResResult modify(@PathVariable("member_no") String memberNo, Member member) {
-        return null;
+    @PostMapping("/modify/{member_no}")
+    public ResResult modify(@PathVariable("member_no") String memberNo, @RequestBody Member member) {
+
+        member.setMemberNo(memberNo);
+        Member m = memberService.modify(member);
+        AssertUtil.notNull(m, "修改失败");
+
+        return ResResult.success("", m);
     }
 
-    @PutMapping("/activate/{member_no}")
-    public ResResult activate(@PathVariable("member_no") String memberNo, boolean active) {
-        return null;
+    @PostMapping("/modifyPassword/{member_no}/{org_pass}/{new_pass}")
+    public ResResult modifyPassword(@PathVariable("member_no") String memberNo, @PathVariable("org_pass") String orgPass, @PathVariable("new_pass") String newPass) {
+
+        boolean ret = memberService.modifyPassword(memberNo, orgPass, newPass);
+        AssertUtil.isTrue(ret, "修改失败");
+
+        return ResResult.success("", ret);
+    }
+
+    @PostMapping("/activate/{member_no}/{active}")
+    public ResResult activate(@PathVariable("member_no") String memberNo, @PathVariable("active") boolean active) {
+
+        boolean ret = memberService.activate(memberNo, active);
+        AssertUtil.isTrue(ret, "修改失败");
+
+        return ResResult.success("", ret);
+    }
+
+    @PostMapping("/query")
+    public ResResult query(@RequestBody MemberQueryDto queryDto) {
+
+        PageInfo<Member> memberList = memberService.findMemberList(queryDto);
+
+        return ResResult.success("", memberList);
+    }
+
+    @GetMapping("/generateNewMemberNo")
+    public ResResult generateNewMemberNo() {
+
+        String ret = memberService.generateNewMemberNo();
+
+        return ResResult.success("生成ID成功", ret);
     }
 }
--- strive-service/src/main/java/com/jacky/strive/service/MemberService.java
+++ strive-service/src/main/java/com/jacky/strive/service/MemberService.java
@@ -1,14 +1,19 @@
 package com.jacky.strive.service;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jacky.strive.dao.KeyValueDao;
 import com.jacky.strive.dao.MemberDao;
-import com.jacky.strive.dao.UserDao;
 import com.jacky.strive.dao.model.Member;
+import com.jacky.strive.service.dto.MemberQueryDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import qsq.biz.common.util.AssertUtil;
+import qsq.biz.common.util.DateUtil;
+import qsq.biz.common.util.Md5Util;
 import qsq.biz.common.util.StringUtil;
-import qsq.biz.scheduler.entity.ResResult;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -28,37 +33,120 @@ public class MemberService {
     @Autowired
     KeyValueDao keyValueDao;
 
+
+    public Member add(Member member) {
+
+        Member u = findByMemberName(member.getMemberMobile());
+        AssertUtil.isTrue(null == u, "手机号已存在");
+
+        member.setCreatedAt(DateUtil.now());
+        member.setMemberPassword(Md5Util.md5Encode(member.getMemberMobile().substring(member.getMemberMobile().length() - 6)));
+        int ret = memberDao.insert(member);
+        return ret > 0 ? member : null;
+    }
+
+    public Member modify(Member member) {
+
+        Member m = findByMemberName(member.getMemberMobile());
+        AssertUtil.isTrue(null == m || m.getMemberNo().equals(member.getMemberNo()), "手机号已存在");
+
+        Example example = new Example(Member.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("memberNo", member.getMemberNo());
+
+        int ret = memberDao.updateByExampleSelective(member, example);
+        return ret > 0 ? member : null;
+    }
+
+    public boolean modifyPassword(String memberNo, String orgPass, String newPass) {
+        Example example = new Example(Member.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("memberNo", memberNo);
+
+        Member member = findByMemberNo(memberNo);
+        AssertUtil.notNull(member, "用户不存在");
+        AssertUtil.isTrue(member.getMemberPassword().equals(orgPass) || member.getMemberPassword().equals(Md5Util.md5Encode(orgPass)),
+                "原密码不正确");
+
+        member.setMemberPassword(Md5Util.md5Encode(newPass));
+
+        int ret = memberDao.updateByExampleSelective(member, example);
+        return ret > 0;
+    }
+
+    public boolean activate(String memberNo, boolean active) {
+        Example example = new Example(Member.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("memberNo", memberNo);
+
+        Member member = findByMemberNo(memberNo);
+        AssertUtil.notNull(member, "用户不存在");
+
+        member.setMemberStatus(active);
+
+        int ret = memberDao.updateByExampleSelective(member, example);
+        return ret > 0;
+    }
+
+    public Member findByMemberName(String memberName) {
+        Example example = new Example(Member.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("memberNo", memberName);
+        criteria.orEqualTo("memberName", memberName);
+        criteria.orEqualTo("memberMobile", memberName);
+
+        Member member = memberDao.selectOneByExample(example);
+        return member;
+    }
+
     public Member findByMemberNo(String memberNo) {
         Example example = new Example(Member.class);
         Example.Criteria criteria = example.createCriteria();
         criteria.andEqualTo("memberNo", memberNo);
-        criteria.orEqualTo("memberName", memberNo);
 
-        Member m = memberDao.selectOneByExample(example);
-        return m;
+        Member member = memberDao.selectOneByExample(example);
+
+        return member;
     }
 
-    public ResResult findAll(String memberNo, String memberName) {
-        return null;
-    }
+    public PageInfo<Member> findMemberList(MemberQueryDto queryDto) {
 
-    public ResResult addMember(Member member) {
-        return null;
-    }
+        Page<Member> page = PageHelper.startPage(queryDto.getPage(), queryDto.getSize());
 
-    public ResResult add(Member member) {
-        return null;
-    }
+        Example example = new Example(Member.class);
+        Example.Criteria criteria1 = example.createCriteria();
+        criteria1.andEqualTo("isActivated", true);
+        criteria1.orIsNull("isActivated");
 
-    public String generateNewMemberID() {
+        Example.Criteria criteria2 = example.createCriteria();
 
-        String maxUserId = keyValueDao.getDynamicResult("SELECT max(member_id) FROM `member`");
-
-        if (!StringUtil.isEmtpy(maxUserId)) {
-            maxUserId = "M" + String.format("%06d", Integer.valueOf(Integer.parseInt(maxUserId.substring(1)) + 1));
-        } else {
-            maxUserId = "M000001";
+        String condition = "";
+        if (null != queryDto.getMemberNo()) {
+            condition = "%" + queryDto.getMemberNo() + "%";
         }
-        return maxUserId;
+
+        criteria2.andLike("memberNo", condition);
+        criteria2.orLike("memberName", condition);
+        criteria2.orLike("memberMobile", condition);
+
+        example.and(criteria2);
+        example.setOrderByClause("created_at desc");
+        List<Member> memberList = memberDao.selectByExample(example);
+
+        PageInfo<Member> pageInfo = new PageInfo<>(memberList);
+
+        return pageInfo;
+    }
+
+    public String generateNewMemberNo() {
+
+        String maxMemberNo = keyValueDao.getDynamicResult("SELECT max(member_no) FROM `member`");
+
+        if (!StringUtil.isEmtpy(maxMemberNo)) {
+            maxMemberNo = "M" + String.format("%06d", Integer.valueOf(Integer.parseInt(maxMemberNo.substring(1)) + 1));
+        } else {
+            maxMemberNo = "M000001";
+        }
+        return maxMemberNo;
     }
 }
